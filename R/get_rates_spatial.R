56683cad5c5771d7f115e86bd37ed368d130d9c7
mattmar <marcantoniomatteo@gmail.com>
2021-02-12 15:28:09 +0100

    Solved an issue in the rate function

--- R/get_rates_spatial.R
+++ R/get_rates_spatial.R
@@ -8,11 +8,14 @@ get_rates_spatial = function(coords=NULL, daily_temp=NULL, species=NULL, rate_fu
 		rate.v=lapply(rate.v, function(x) round(1/x))
 	}
 
-	if(spatial) rate.ras <- stack(mclapply(rate.v,function(x) {
-		rate.sp <- cbind.data.frame(X=coords[,1],Y=coords[,2],Var=x)
-		coordinates(rate.sp) <- ~X+Y
-		gridded(rate.sp) <- TRUE
-		raster(rate.sp)
+	if(spatial) {
+		rate.ras <- stack(mclapply(rate.v,function(x) {
+			rate.sp <- cbind.data.frame(X=coords[,1],Y=coords[,2],Var=x)
+			coordinates(rate.sp) <- ~X+Y
+			gridded(rate.sp) <- TRUE
+			raster(rate.sp)
+		}, mc.cores=np))
+		names(rate.ras) <- paste0("d",1:dim(rate.ras)[3])
 		return(rate.ras)
-	}, mc.cores=np)) else(return(do.call(cbind.data.frame, rate.v)))
+	} else(return(do.call(cbind.data.frame, rate.v)))
 }
