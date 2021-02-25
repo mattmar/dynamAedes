82a36ca59d8106784387fc0829432d539b96dfc5
mattmar <marcantoniomatteo@gmail.com>
2021-02-25 16:54:23 +0100

    named life table array and multicore psi_sp()

--- R/dynamAedes.R
+++ R/dynamAedes.R
@@ -96,8 +96,8 @@ dynamAedes <- function(species="aegypti", scale="ws", ihwv=1, temps.matrix=NULL,
 					e.intro.n <- intro.eggs; i.intro.n <- intro.immatures; a.intro.n <- intro.adults; road.dist.matrix <- as.data.frame(c(0,0)); names(road.dist.matrix) <- 1
 				}
 			} else stop("Wrong scale.")
-        ### Day cycle: Start sequential "day" life cycle into the "iteration" loop ###
-		## Define counter which serves as an introduction benchmark
+        	### Day cycle: Start sequential "day" life cycle into the "iteration" loop ###
+			## Define counter which serves as an introduction benchmark
 			if( exists("counter") ) {
 				rm(counter)
 			}
@@ -105,100 +105,100 @@ dynamAedes <- function(species="aegypti", scale="ws", ihwv=1, temps.matrix=NULL,
 			foreach(day = startd:endd, .combine=c, .export = ls(globalenv())) %do% {
 				if( !stopit ) {
 					if( !exists("counter") ) {
-                    # Define objects required to store data during a day
-						counter <- 0; i.surv.m <- matrix(0,ncol=5,nrow=2); i.temp.v <- 0; d.temp.v <- 0; e.temp.v <- 0; a.egg.n <- 0; a.degg.n <- 0; p.life.a <- array(0,c(4,nrow(temps.matrix),6)); storage.mode(p.life.a) <- "integer"; outl <- list()
+                    	# Define objects required to store data during a day
+						counter <- 0; i.surv.m <- matrix(0,ncol=5,nrow=2); i.temp.v <- 0; d.temp.v <- 0; e.temp.v <- 0; a.egg.n <- 0; a.degg.n <- 0; p.life.a <- array(0,c(4,nrow(temps.matrix),6), dimnames = list(c("egg", "juvenile", "adult", "diapause_egg"), NULL, c("sc1", "sc2", "sc3", "sc4", "sc5", "sc6"))); storage.mode(p.life.a) <- "integer"; outl <- list()
 					} else counter <- append(counter,day)
 
-                ### Header:
-                ## Gonotrophic cycle
-                ## Derive daily rate for gonotrophic cycle, i.e. blood meal to oviposition, then transform rate in daily probabiltiy to terminate the gonotrophic cycle.
+                	### Header:
+                	## Gonotrophic cycle
+                	## Derive daily rate for gonotrophic cycle, i.e. blood meal to oviposition, then transform rate in daily probabiltiy to terminate the gonotrophic cycle.
 					a.gono.p <- .a.gono_rate.f(temps.matrix[,day]/1000, species)
-                ## Oviposition rate
-                ## Derive oviposition rate, i.e. number of eggs laid per female per day.
+                	## Oviposition rate
+                	## Derive oviposition rate, i.e. number of eggs laid per female per day.
 					a.batc.n <- .a.ovi_rate.f(temps.matrix[,day]/1000, species)
-                ## Adult survival
-                ## Derive daily adult female survival rate and transform rate in daily probabiltiy to survive.
+                	## Adult survival
+                	## Derive daily adult female survival rate and transform rate in daily probabiltiy to survive.
 					a.surv.p <- .a.surv_rate.f(temps.matrix[,day]/1000, species)
-                ## Immature emergence
-                ## Derive daily immature emergence rate then transform rate in daily probabiltiy to emerge.
+                	## Immature emergence
+                	## Derive daily immature emergence rate then transform rate in daily probabiltiy to emerge.
 					i.emer.p <- .i.emer_rate.f(temps.matrix[,day]/1000, species)
-                ## Immature survival
-                ## Derive daily immature survival rate
+                	## Immature survival
+                	## Derive daily immature survival rate
 					i.mort_rate.v <- -log(.i.surv_rate.f(temps.matrix[,day]/1000, species))
-                ## Derive daily egg hatching rate then transform rate in daily probabiltiy to hatch.
+                	## Derive daily egg hatching rate then transform rate in daily probabiltiy to hatch.
 					e.hatc.p <- .e.hatch_rate.f(temps.matrix[,day]/1000, species)
-                ## Derive daily egg survival rate then transform rate in daily probabiltiy to survive.
+                	## Derive daily egg survival rate then transform rate in daily probabiltiy to survive.
 					e.surv.p <- .e.surv_rate.f(temps.matrix[,day]/1000, species)
 					d.surv.p <- if(species=="albopictus"){
 						.d.surv_rate.f(temps.matrix[,day]/1000)
 					} else 0
-				# Binned (10m) (Log-normal) probability density for active dispersal up to 600m 
+					# Binned (10m) (Log-normal) probability density for active dispersal up to 600m 
 					if(dispersal) {f.adis.p <- .a.a_disp.f(sp=species)}
-				## Gamma probability density of long passive dispersal (from DOI: 10.2790/7028); from 0 to maximum distance of road segments with 1000 m resolution.
+					## Gamma probability density of long passive dispersal (from DOI: 10.2790/7028); from 0 to maximum distance of road segments with 1000 m resolution.
 					if(dispersal) {f.pdis.p <- dgamma(seq(1,max(road.dist.matrix,na.rm=T),1000),shape=car.avg.trip/(10000/car.avg.trip), scale=10000/car.avg.trip)}
-                ### Events in the (`E`) egg compartment
-                ## `E` has four sub-compartment: 1:3 for eggs 1-3 days old that can only die or survive, 4 for eggs older than 3 days that can die/survive/hatch
-                ## Binomial draw to find numbers of eggs that die or survive
+                	### Events in the (`E`) egg compartment
+                	## `E` has four sub-compartment: 1:3 for eggs 1-3 days old that can only die or survive, 4 for eggs older than 3 days that can die/survive/hatch
+                	## Binomial draw to find numbers of eggs that die or survive
 					p.life.a[1,,2:4] <- apply(t(p.life.a[1,,1:3]),MARGIN=mrg,function(x) rbinom(size=x,n=space,prob=e.surv.p))
 					p.life.a[4,,2:4] <- apply(t(p.life.a[4,,1:3]),MARGIN=mrg,function(x) rbinom(size=x,n=space,prob=d.surv.p))
-                ## Introduce eggs if day==1; introduction happens in E sub-compartment 4 as it can be assumed that eggs are most likely to be introduced in an advanced stage of development 
+                	## Introduce eggs if day==1; introduction happens in E sub-compartment 4 as it can be assumed that eggs are most likely to be introduced in an advanced stage of development 
 					p.life.a[1,,4] <- if( length(counter)==1 ) {
 						e.intro.n
 					} else p.life.a[1,,4]
-                # Add eggs laid by females the day before (t-1) stored in a.egg.n (end of the day)
+                	# Add eggs laid by females the day before (t-1) stored in a.egg.n (end of the day)
 					p.life.a[1,,1] <- a.egg.n
 					p.life.a[4,,1] <- a.degg.n
-                # Add eggs that did not hatch yesterday to egg that today are ready to hatch
+                	# Add eggs that did not hatch yesterday to egg that today are ready to hatch
 					p.life.a[1,,4] <- p.life.a[1,,4] + e.temp.v
 					p.life.a[4,,4] <- p.life.a[4,,4] + d.temp.v
-                # Binomial draw to find numbers of eggs 4-day+ old that hatch today
+                	# Binomial draw to find numbers of eggs 4-day+ old that hatch today
 					e.hatc.n <- rbinom(length(1:space), p.life.a[1,,4], prob=e.hatc.p)
 					d.hatc.n <- rbinom(length(1:space), p.life.a[4,,4], prob=e.hatc.p)
-                # Remove hatched eggs from eggs 4d+ old
+                	# Remove hatched eggs from eggs 4d+ old
 					e.temp.v <- p.life.a[1,,4] - e.hatc.n
 					d.temp.v <- p.life.a[4,,4] - d.hatc.n
-                # Apply mortality to non hatched 4d+ old eggs
+                	# Apply mortality to non hatched 4d+ old eggs
 					e.temp.v <- rbinom(length(1:space), e.temp.v, prob=0.99)
 					d.temp.v <- rbinom(length(1:space), d.temp.v, prob=0.99)
-                ### Events in the (`I`) immature compartment
-                ## `I` has 6 sub-compartments representing days from hatching; an immature can survive/die for the first 5 days after hatching, from the 5th day on, it can survive/die and `emerge`.
-                ## Derive mortality rate due to density and add to mortality rate due to temperature sum and derive probability of survival in each cell.
+	                ### Events in the (`I`) immature compartment
+	                ## `I` has 6 sub-compartments representing days from hatching; an immature can survive/die for the first 5 days after hatching, from the 5th day on, it can survive/die and `emerge`.
+	                ## Derive mortality rate due to density and add to mortality rate due to temperature sum and derive probability of survival in each cell.
 					imm.v <- if(scale=="ws") {
 						sum(p.life.a[2,,2:6])
 					} else rowSums(p.life.a[2,,2:6])
-				## Derive density-dependent mortality,*2 is to report densities at 1L (original model is for a 2L water habitat.) / ihwv transform density to new liter/cell habitat volume
+					## Derive density-dependent mortality,*2 is to report densities at 1L (original model is for a 2L water habitat.) / ihwv transform density to new liter/cell habitat volume
 					i.ddmort_rate.v <- exp(.i.ddmort_rate.f(list(i.dens.v=(imm.v*2)/ihwv)))
 					i.surv.p <- 1-(1-exp(-(i.mort_rate.v + i.ddmort_rate.v)))
-                ## Binomial draw to find numbers of immature that die or survive-and-move to the next compartment
+                	## Binomial draw to find numbers of immature that die or survive-and-move to the next compartment
 					p.life.a[2,,2:6] <- apply(t(p.life.a[2,,1:5]), MARGIN=mrg, FUN=function(x) rbinom(size=x, n=space, prob=i.surv.p))
-                ## Introduce `I` if day==1; introduction happens in `I` sub-compartment 6 
+                	## Introduce `I` if day==1; introduction happens in `I` sub-compartment 6 
 					p.life.a[2,,6] <- if( length(counter)==1 ) {
 						i.intro.n
 					} else p.life.a[2,,6]
-                ## Add immatures hatched the same day
+                	## Add immatures hatched the same day
 					p.life.a[2,,1] <- e.hatc.n + d.hatc.n
-                ## Add immatures that did not emerge yesterday to immatures that today are ready to emerge
+                	## Add immatures that did not emerge yesterday to immatures that today are ready to emerge
 					p.life.a[2,,6] <- p.life.a[2,,6] + i.temp.v
-                ## Find numbers of immature 5d+ old that emerge before applying mortality (applied as newly emerged adults today)
+                	## Find numbers of immature 5d+ old that emerge before applying mortality (applied as newly emerged adults today)
 					i.emer.n <- rbinom(length(1:space), p.life.a[2,,6], prob=i.emer.p)
-                ## Remove emerged immatures from immatures 5d+ old
+                	## Remove emerged immatures from immatures 5d+ old
 					i.temp.v <- p.life.a[2,,6] - i.emer.n
-                ## Apply mortality to non emerged 5d+ old immatures
+                	## Apply mortality to non emerged 5d+ old immatures
 					i.temp.v <- sapply(1:space, function(x){rbinom(1,i.temp.v[x],prob=i.surv.p[x])})
-                ### Events in the (`A`) adult compartment
-                ## `A` has 5 sub-compartments representing: adults in day 1 and 2 of oviposition [2:3]; 2d+ old adults host-seeking and non ovipositing [4]; 2d+ old blod-fed adults which are not yet laying [1]; 1d old adults, non-laying and non-dispersing [5].
-                ## Introduce blood-fed females if day is 1
+                	### Events in the (`A`) adult compartment
+                	## `A` has 5 sub-compartments representing: adults in day 1 and 2 of oviposition [2:3]; 2d+ old adults host-seeking and non ovipositing [4]; 2d+ old blod-fed adults which are not yet laying [1]; 1d old adults, non-laying and non-dispersing [5].
+                	## Introduce blood-fed females if day is 1
 					p.life.a[3,,1] <- if( length(counter)==1 ) {
 						a.intro.n
 					} else p.life.a[3,,1]
-                ## Binomial random draw to remove males adult from newly emerged adults
+                	## Binomial random draw to remove males adult from newly emerged adults
 					p.life.a[3,,5] <- rbinom(space,i.emer.n, prob=0.5)
-                ## Add blood-fed females which today matured eggs to females with matured eggs from yesteday 
+                	## Add blood-fed females which today matured eggs to females with matured eggs from yesteday 
 					n.ovir.a <- rbinom(space, p.life.a[3,,1], prob=a.gono.p)
 					p.life.a[3,,2] <- n.ovir.a
-                ## Remove females which today matured eggs from the host-fed compartment
+                	## Remove females which today matured eggs from the host-fed compartment
 					p.life.a[3,,1] <- p.life.a[3,,1] - n.ovir.a
-                ## Find number of eggs laid today by ovipositing females
+                	## Find number of eggs laid today by ovipositing females
 					if( species=="albopictus" & dl[day]<=11.0 ) {
 						if(verbose) print("Laying diapausing eggs")
 							a.degg.n <- sapply(1:space, function(x) sum(rpois(sum(p.life.a[3,x,2:3]), a.batc.n[x])))
@@ -207,25 +207,25 @@ dynamAedes <- function(species="aegypti", scale="ws", ihwv=1, temps.matrix=NULL,
 						a.egg.n <- sapply(1:space, function(x) sum(rpois(sum(p.life.a[3,x,2:3]), a.batc.n[x])))
 						a.degg.n=0
 					}
-                ## Find number of adult females surviving today
+                	## Find number of adult females surviving today
 					p.life.a[3,,1:5] <- apply(t(p.life.a[3,,1:5]),MARGIN=mrg,FUN=function(x) rbinom(size=x,n=space,prob=a.surv.p))
-                ## Short-distance active dispersal
+                	## Short-distance active dispersal
 					if( dispersal ) {
-                	# It happens only if there is any host-seeking female [4], which are the only actively dispersing
+                		# It happens only if there is any host-seeking female [4], which are the only actively dispersing
 						if( any(which(p.life.a[3,,4]>0)) ) {
-                    	# Find cells (origin) which have at least 1 host-seeking dispersing female
+                    		# Find cells (origin) which have at least 1 host-seeking dispersing female
 							f.ocel.v <- which(p.life.a[3,,4]>0)
-                    	# Find how many host-seeking females move and at what distance (0-600 m - every 10 m)
+                    		# Find how many host-seeking females move and at what distance (0-600 m - every 10 m)
 							f.adis.v <- sapply(f.ocel.v, function(x) rmultinom(1,p.life.a[3,x,4],f.adis.p))
-                    	# Find a landing cell for each distance at which each set of adult females from the same origin disperses to.
+                    		# Find a landing cell for each distance at which each set of adult females from the same origin disperses to.
 							for (i in 1:length(f.ocel.v)) {
-                        	# Get cell of origin from cell index
+                        		# Get cell of origin from cell index
 								e <- f.ocel.v[i]
-                        	# Build a distance matrix between cell of origin and all other cells in the grid; round with option -1 returns the dispersal distance to the closest ten to match the resolution of the dispersal kernel. 
+                        		# Build a distance matrix between cell of origin and all other cells in the grid; round with option -1 returns the dispersal distance to the closest ten to match the resolution of the dispersal kernel. 
 								cell.dist.matrix <- sapply(rdist(cells.coords[e,],cells.coords,compact=T),function(x) round(x,-1))
-                        	# Set matrix columns as the number of the row in the grid
+                        		# Set matrix columns as the number of the row in the grid
 								names(cell.dist.matrix) <- 1:nrow(cells.coords)
-                        	# Find set of dispersing cells for each `origin` based on the selected dispersing distances; any mosquito that disperses <250 m stays in the cell of origin. Note: *10-10 is to transform the cell id (1-60) to 100-600 to match the range of the dispersal kernel and set the first dispersal distance to 0 (not 1).
+                        		# Find set of dispersing cells for each `origin` based on the selected dispersing distances; any mosquito that disperses <250 m stays in the cell of origin. Note: *10-10 is to transform the cell id (1-60) to 100-600 to match the range of the dispersal kernel and set the first dispersal distance to 0 (not 1).
 								a.plan.l <- sapply(which(f.adis.v[,i]<max(cell.dist.matrix))*10-10, function(x) {
 									if( x<cellsize ) {
 										x <- as.numeric()
@@ -233,20 +233,20 @@ dynamAedes <- function(species="aegypti", scale="ws", ihwv=1, temps.matrix=NULL,
 										x<-cell.dist.matrix[as.numeric(which(cell.dist.matrix==x))]
 									}; return(x)
 								})
-                        	# Set dispersing distances which do not exist in the distance matrix (cell.dist.matrix) as NULL; for example distances > 600, maximum of the dispersal kernel
+                        		# Set dispersing distances which do not exist in the distance matrix (cell.dist.matrix) as NULL; for example distances > 600, maximum of the dispersal kernel
 								a.plan.l <- lapply(a.plan.l, function(x) if( (length(x)>0) & (is.null(names(x)) | any(is.na(names(x)))) ) {
 									x=numeric()
 								} else x)
-                        	# Randomly choose a landing cell for each dispersing distance, thus the direction of dispersal is completely random within each distance class (non directional or anisotrophic dispersal). Set cells not selected for landing as -999
+                        		# Randomly choose a landing cell for each dispersing distance, thus the direction of dispersal is completely random within each distance class (non directional or anisotrophic dispersal). Set cells not selected for landing as -999
 								a.land.v <- sapply(a.plan.l, function(x) {if( length(x)>0 ) {
 									.resample(x,1,replace=FALSE)
 								} else -999})
 								toret <- as.integer(which(a.land.v>=0))
-                        	# If there is any dispersal distance farther than 0 (alias >250 m, see above) for a cell of origin, then proceed with moving dispersing females from origin to landing cells
+                        		# If there is any dispersal distance farther than 0 (alias >250 m, see above) for a cell of origin, then proceed with moving dispersing females from origin to landing cells
 								if( length(f.adis.v[which(f.adis.v[,i]<max(cell.dist.matrix)),i][toret])>0 ) {
-                        		# Remove dispersing individuals from `origin`
+                        			# Remove dispersing individuals from `origin`
 									p.life.a[3,e,4] <- p.life.a[3,e,4] - sum(f.adis.v[which(f.adis.v[,i]<max(cell.dist.matrix)),i][toret])
-                            	# Add dispersing individuals in the chosen landing cell for each distance
+                            		# Add dispersing individuals in the chosen landing cell for each distance
 									p.life.a[3,as.integer(names(a.land.v[toret])),4] <- p.life.a[3,as.integer(names(a.land.v[toret])),4] + f.adis.v[which(f.adis.v[,i]<max(cell.dist.matrix)),i][toret]
 								} else if(verbose) print("Actively dispersing females stay in the cell of origin. Jumping to the next cell of origin...")
 							}
@@ -297,7 +297,7 @@ dynamAedes <- function(species="aegypti", scale="ws", ihwv=1, temps.matrix=NULL,
 					p.life.aout <- if( compressed.output ) {
 						apply(p.life.a, MARGIN=c(1, 2), sum)
 					} else { 
-						p.life.aout <- p.life.a 
+						p.life.a							
 					}
                 	# If TRUE a sparse array is returned (save memory but complex to process)
 					if(sparse.output) return(list(as.simple_sparse_array(p.life.a))) else return(list(p.life.aout))
--- R/psi_sp.R
+++ R/psi_sp.R
@@ -1,22 +1,22 @@
-psi_sp = function(coords=NULL, input_sim=NULL, eval_date=NULL) {
+psi_sp = function(coords=NULL, input_sim=NULL, eval_date=NULL, np=1) {
 	if( all(unlist(lapply(input_sim, function(x) { sapply(x,length) } ))==4) ) {
 		stop("Non-spatial data, set scale='lc' or scale='rg' in dynamAedes")
 	} else {
 		if( max(eval_date) > max(sapply(input_sim,length)) ) {
 			stop("eval_date > than maximum number of simulated days across all iterations...")
 		} else {
-			mylist <- lapply(input_sim, function(x){
+			mylist <- mclapply(input_sim, function(x){
 				mydf <- lapply(x, function(y) {data.frame(y); data.frame("tot_individuals"= colSums(y))});
 				mydf <- do.call(cbind, mydf)
-			})
+			}, mc.cores=np)
 			maxl <- max(sapply(mylist,ncol))
-			mylist <- lapply(mylist, function(x) {
+			mylist <- mclapply(mylist, function(x) {
 				if(length(x)<maxl) {
 					cbind.data.frame( x, cbind.data.frame( matrix(rep(rep(0,nrow(x)),(maxl-length(x))), ncol=(maxl-length(x)))) )
 				} else {
 					x
 				}
-			})
+			},mc.cores=np)
   			#add days and convert to 1 all pixel having a number of individuals >1
 			col_names <- paste0( "d_", 1:maxl )
 			mylist <- lapply(mylist, setNames, col_names)
