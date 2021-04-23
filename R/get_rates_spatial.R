309f9916ed8c02a3366da6c033cc338e0dd93632
danddr <37628753+danddr@users.noreply.github.com>
2021-04-23 15:06:03 +0200

    Update get_rates_spatial.R
    update rasterization using raster::rasterFromXYZ()

--- R/get_rates_spatial.R
+++ R/get_rates_spatial.R
@@ -11,9 +11,7 @@ get_rates_spatial = function(coords=NULL, daily_temp=NULL, species=NULL, rate_fu
 	if(spatial) {
 		rate.ras <- stack(mclapply(rate.v,function(x) {
 			rate.sp <- cbind.data.frame(X=coords[,1],Y=coords[,2],Var=x)
-			sp::coordinates(rate.sp) <- ~X+Y
-			sp::gridded(rate.sp) <- TRUE
-			raster(rate.sp)
+			rate.sp<-rasterFromXYZ(rate.sp)
 		}, mc.cores=np))
 		names(rate.ras) <- paste0("d",1:dim(rate.ras)[3])
 		return(rate.ras)
